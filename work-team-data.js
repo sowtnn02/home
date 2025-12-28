67e5aa9f6bcaecec44132572626fb02a01a9543a
sowtnn02 <tnn02@sowfamily.org>
2025-12-28 19:22:20 +0800

    Add files via upload

---
+++ work-team-data.js
@@ -0,0 +1,94 @@
+const workteamData = {
+  ant: {
+    "小蟻團導引團隊": [
+      { title: "複式團團長", family: "18 橘子皮 / 橘子樹", volunteer: "橘子皮" },
+      { title: "團長", family: "41 龍葵", volunteer: "龍葵" },
+      { title: "副團長", family: "37 風神翼龍 / 小葉欖仁", volunteer: "小葉欖仁" },
+      { title: "小黑蟻 導引員", family: "48 折衷鸚鵡 / 海鸚", volunteer: "海鸚" },
+      { title: "小黑蟻 導引員", family: "61 薰衣草", volunteer: "薰衣草" },
+      { title: "小黃蟻 導引員", family: "56 角鯨 / 海鷗", volunteer: "角鯨" },
+      { title: "小黃蟻 導引員", family: "60 森林 / 螢火蟲", volunteer: "螢火蟲" },
+      { title: "小綠蟻 導引員", family: "39 燈籠果 / 桔梗", volunteer: "桔梗" },
+      { title: "小綠蟻 導引員", family: "62 落羽松", volunteer: "落羽松" },
+      { title: "小紅蟻 導引員", family: "44 米蝦 / 珊瑚大戟", volunteer: "珊瑚大戟" },
+      { title: "小紅蟻 導引員", family: "51 水熊蟲 / 橘小豬", volunteer: "橘小豬" }
+    ],
+    "小蟻團育成會團隊": [
+      { title: "會長", family: "17 咸豐草 / 菱角", volunteer: "菱角" },
+      { title: "副會長", family: "47 海峽 / 野薑花", volunteer: "野薑花" },
+      { title: "小黑蟻 育導", family: "53 松樹 / 松鼠", volunteer: "松鼠" },
+      { title: "小黃蟻 育導", family: "55 Hyena / 銀合歡", volunteer: "銀合歡" },
+      { title: "小綠蟻 育導", family: "52 海膽 / 芒草", volunteer: "芒草" },
+      { title: "小紅蟻 育導", family: "46 冬至 / 瓢蟲", volunteer: "瓢蟲" },
+      { title: "財務", family: "67 腕龍 / 荔枝", volunteer: "荔枝" },
+      { title: "攝影", family: "34 泥鰍 / 蘆薈", volunteer: "泥鰍" },
+      { title: "攝影", family: "28 山 / 秋", volunteer: "秋" },
+      { title: "攝影", family: "58 管鬚蟹 / 藍腹鷴", volunteer: "管鬚蟹" },
+      { title: "攝影", family: "64 山羊 / 拉布拉多", volunteer: "拉布拉多" }
+    ]
+  },
+  bee: {
+    "炫蜂團導引團隊": [
+      { title: "複式團團長", family: "18 橘子皮 / 橘子樹", volunteer: "橘子皮" },
+      { title: "團長", family: "23 鋰(Li) / 鎂(Mg)", volunteer: "鋰" },
+      { title: "副團長", family: "4 水熊 / 玉露", volunteer: "玉露" },
+      { title: "泥壺蜂 導引員", family: "31 Camel 駱駝 / 玄鳳", volunteer: "玄鳳" },
+      { title: "泥壺蜂 導引員", family: "63 大草 / 無尾熊", volunteer: "無尾熊" },
+      { title: "虎頭蜂 導引員", family: "46 冬至 / 瓢蟲", volunteer: "冬至" },
+      { title: "虎頭蜂 導引員", family: "57 山櫻花", volunteer: "山櫻花" },
+      { title: "長腳蜂 導引員", family: "28 山 / 秋", volunteer: "山" },
+      { title: "長腳蜂 導引員", family: "42 小麥穗 / 尤加利", volunteer: "尤加利" },
+      { title: "細腰蜂 導引員", family: "22 蟑螂 / 石榴", volunteer: "石榴" },
+      { title: "細腰蜂 導引員", family: "69 魟魚 / 波波草", volunteer: "魟魚" }
+    ],
+    "炫蜂團育成會團隊": [
+      { title: "會長", family: "17 咸豐草 / 菱角", volunteer: "菱角" },
+      { title: "副會長", family: "43 袋鼠 / 小羽苔", volunteer: "小羽苔" },
+      { title: "泥壺蜂 育導", family: "38 麥香 / 玉如意", volunteer: "玉如意" },
+      { title: "虎頭蜂 育導", family: "59 兔袋鼠 / 香草莢", volunteer: "兔袋鼠" },
+      { title: "長腳蜂 育導", family: "26 海獅 / 海狗", volunteer: "海狗" },
+      { title: "細腰蜂 育導", family: "63 大草 / 無尾熊", volunteer: "大草" },
+      { title: "財務", family: "40 極光 / 藍洞", volunteer: "藍洞" },
+      { title: "攝影", family: "66 螃蟹 / 水豚", volunteer: "螃蟹" },
+      { title: "攝影", family: "24 星螺 / 淡菜", volunteer: "淡菜" },
+      { title: "攝影", family: "54 獅子王 / 三色堇", volunteer: "獅子王" },
+      { title: "攝影", family: "36 太陽 / 銀河", volunteer: "銀河" },
+      { title: "攝影", family: "25 臭氧層 / 香氣", volunteer: "臭氧層" },
+      { title: "攝影", family: "12 北極熊 / 企鵝", volunteer: "企鵝" }
+    ]
+  },
+  deer: {
+    "奔鹿團導引團隊": [
+      { title: "複式團團長", family: "18 橘子皮 / 橘子樹", volunteer: "橘子皮" },
+      { title: "團長", family: "19 隼 / 苦楝", volunteer: "隼" },
+      { title: "副團長", family: "45 鯨頭鸛 / 草履蟲", volunteer: "鯨頭鸛" },
+      { title: "草原鹿 導引員", family: "50 稻穗 / 上弦月", volunteer: "稻穗" },
+      { title: "森林鹿", family: "65 玉山 / 北極狐", volunteer: "玉山" },
+      { title: "高地鹿", family: "3 河馬 / 銀杏", volunteer: "銀杏" },
+      { title: "湖泊鹿", family: "27 咬人貓 / 嬰兒淚", volunteer: "嬰兒淚" }
+    ],
+    "奔鹿團育成會團隊": [
+      { title: "會長", family: "17 咸豐草 / 菱角", volunteer: "菱角" },
+      { title: "副會長", family: "21 藍眼淚 / 響鈴豆", volunteer: "響鈴豆" },
+      { title: "草原鹿 育導", family: "21 藍眼淚 / 響鈴豆", volunteer: "響鈴豆" },
+      { title: "森林鹿 育導", family: "15 石虎 / 藍花藤", volunteer: "藍花藤" },
+      { title: "高地鹿 育導", family: "33 泥巴 / 蘆葦", volunteer: "蘆葦" },
+      { title: "湖泊鹿 育導", family: "68 水牛 / 水竹", volunteer: "水竹" },
+      { title: "財務", family: "11 阿勃勒 / 楓樹", volunteer: "楓樹" },
+      { title: "攝影", family: "70 晨光 / 麋鹿", volunteer: "晨光" },
+      { title: "攝影", family: "32 ICE / 栗子", volunteer: "ICE" }
+    ]
+  },
+  eagle: {
+    "翔鷹團導引團隊": [
+      { title: "複式團團長", family: "18 橘子皮 / 橘子樹", volunteer: "橘子皮" },
+      { title: "團長", family: "10 白頭翁 / 牡羊座", volunteer: "白頭翁" },
+      { title: "副團長（小鷹）", family: "2 西洋山葵 / 檸檬", volunteer: "櫻花" }
+    ],
+    "翔鷹團育成會團隊": [
+      { title: "會長", family: "17 咸豐草 / 菱角", volunteer: "菱角" },
+      { title: "副會長", family: "6 黑熊 / 土豆仁", volunteer: "土豆仁" },
+      { title: "財務", family: "5 長臂猿 / 孔雀魚", volunteer: "孔雀魚" }
+    ]
+  }
+};
