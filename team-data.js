c889af5eb3d33927d339bdc078f4c1d62ec9f5fe
sowtnn02 <tnn02@sowfamily.org>
2026-07-24 04:44:30 +0800

    Update team-data.js

--- team-data.js
+++ team-data.js
@@ -23,13 +23,13 @@ const teamData = {
       { family: "46 冬至 / 瓢蟲", child: "小美人魚" },
       { family: "47 海峽 / 野薑花", child: "小石虎" },
       { family: "56 角鯨 / 海鷗", child: "小蝙蝠" },
-      { family: "71 海岸線 / 星辰花", child: "小金魚" }, // 🟢 已修正：從小黑蟻移過來
+      { family: "71 海岸線 / 星辰花", child: "小金魚" },
       { family: "97 地瓜 / 暗光鳥", child: "小馬" },
       { family: "98 土星 / 海棠", child: "珍珠" }
     ],
     "小紅蟻": [
       { family: "44 米蝦 / 珊瑚大戟", child: "捕蠅草" },
-      { family: "61 薰衣草 / 花園鰻", child: "花園鰻" }, // 🟢 已修正：從小綠蟻移過來，請確認家庭名稱格式
+      { family: "61 薰衣草 / 花園鰻", child: "花園鰻" },
       { family: "81 狗尾草 / 卡斯比亞", child: "小樹懶" },
       { family: "82 白臘樹 / 山茶花", child: "南瓜" },
       { family: "89 龜殼花 / 草莓角蛙", child: "草莓角蛙" },
@@ -82,4 +82,53 @@ const teamData = {
       { family: "93 雨 / 陽光", child: "天空" }
     ]
   },
-  // deer 與 eagle 保持原樣...
+  deer: {
+    "草原鹿": [
+      { family: "68 水牛 / 水竹", child: "蜂鳥" },
+      { family: "19 隼 / 苦楝", child: "黑貓" },
+      { family: "33 泥巴 / 蘆葦", child: "藪貓" },
+      { family: "70 晨光 / 麋鹿", child: "美洲虎" },
+      { family: "3 河馬 / 銀杏", child: "小海馬" },
+      { family: "27 咬人貓 / 嬰兒淚", child: "天堂鳥" }
+    ],
+    "森林鹿": [
+      { family: "74 跑山雞 / 黃花風鈴木", child: "山獅" },
+      { family: "17 咸豐草 / 菱角", child: "帝雉" },
+      { family: "19 隼 / 苦楝", child: "笑翠鳥" },
+      { family: "11 阿勃勒 / 楓樹", child: "牽牛花" },
+      { family: "18 橘子皮 / 橘子樹", child: "橘子葉" }
+    ],
+    "高地鹿": [
+      { family: "15 石虎 / 藍花藤", child: "風信子" },
+      { family: "21 藍眼淚 / 響鈴豆", child: "白頭鷹" },
+      { family: "45 鯨頭鸛 / 草履蟲", child: "小獅子" },
+      { family: "77 桂冠草 / 北極星", child: "花栗鼠" },
+      { family: "69 魟魚 / 波波草", child: "海桐" },
+      { family: "50 稻穗 / 上弦月", child: "鸚鵡螺" }
+    ],
+    "湖泊鹿": [
+      { family: "65 玉山 / 北極狐", child: "貓熊" },
+      { family: "12 北極熊 / 企鵝", child: "虎鯨" },
+      { family: "69 魟魚 / 波波草", child: "綠竹" },
+      { family: "32 ICE / 栗子", child: "小刺蝟" },
+      { family: "86 石斑 / 丹楓", child: "波妞" }
+    ]
+  },
+  eagle: {
+    "翔鷹": [
+      { family: "1 鳳凰 / 米蟲", child: "夏天" },
+      { family: "2 西洋山葵 / 檸檬", child: "櫻花" },
+      { family: "5 長臂猿 / 孔雀魚", child: "小蜜蜂" },
+      { family: "6 黑熊 / 土豆仁", child: "鼴鼠" },
+      { family: "7 錦鯉 / 曼波魚", child: "小魚兒" },
+      { family: "8 灰熊 / 相思樹", child: "含羞草" },
+      { family: "8 灰熊 / 相思樹", child: "穿山甲" },
+      { family: "9 藍鯨 / 向日葵", child: "犀牛" },
+      { family: "10 白頭翁 / 牡羊座", child: "小雨燕" },
+      { family: "13 鯨魚 / 山竹", child: "白熊" },
+      { family: "14 石頭 / 毛西番蓮", child: "小螳螂" },
+      { family: "20 櫻花鹿", child: "梅花兔" },
+      { family: "88 獅子座 / 青鳥", child: "台灣黑熊" }
+    ]
+  }
+};
